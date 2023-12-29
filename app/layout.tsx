5bfbd82e2b08fd7bab4d4e4ab71b7eb51eeb3265
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2023-12-29 21:54:43 +0000

    :construction: scaffold

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import WalletContextProvider from './_components/WalletContextProvider'
+import { ButtonAppBar } from './_components/AppBar'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +21,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <WalletContextProvider>
+      <html lang="en">
+        <body>
+          <ButtonAppBar />
+          {children}
+        </body>
+      </html>
+    </WalletContextProvider>
   )
 }
